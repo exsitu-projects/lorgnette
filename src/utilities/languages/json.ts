30de2c3609fadc637607ea414083ec22a654dce8
Daru13 <daru14@gmail.com>
2022-07-13 19:29:39 +0200

    Implement the style inspector for Vega-Lite marks

---
+++ src/utilities/languages/json.ts
@@ -0,0 +1,136 @@
+import { Document } from "../../core/documents/Document";
+import { DocumentEditor } from "../../core/documents/DocumentEditor";
+import { Range } from "../../core/documents/Range";
+import { ObjectNode } from "../../core/languages/json/nodes/ObjectNode";
+import { PropertyNode } from "../../core/languages/json/nodes/PropertyNode";
+
+// Search for the index of a property in a JSON object and returns it (or -1 if none were found).
+// If several property names are specified, each name is compared against all the keys,
+// one name after the other, until a match is found or all the names have been used.
+export function findMatchingPropertyIndex(
+    object: ObjectNode,
+    propertyNameOrNames: string | string[]
+): number {
+    const propertyNames = Array.isArray(propertyNameOrNames) ? propertyNameOrNames : [propertyNameOrNames];
+
+    for (let propertyName of propertyNames) {
+        const matchingPropertyIndex = object.properties.findIndex(p => p.key.value === propertyName);
+        if (matchingPropertyIndex >= 0) {
+            return matchingPropertyIndex;
+        }
+    }
+
+    return -1;
+}
+
+// Search for a property in a JSON object and returns it (or undefined if none were found).
+// If several property names are specified, see findMatchingPropertyIndex for details on how they are used.
+export function findMatchingProperty(
+    object: ObjectNode,
+    propertyNameOrNames: string | string[]
+): PropertyNode | undefined {
+    const propertyIndex = findMatchingPropertyIndex(object, propertyNameOrNames);
+    return propertyIndex >= 0 ? object.properties[propertyIndex] : undefined;
+}
+
+// Utility to search for a property in a JSON object and perform
+// one of two actions depending on whether it exists or not.
+export function processProperty(
+    object: ObjectNode,
+    propertyNameOrNames: string | string[],
+    actionIfPropertyIsPresent: (property: PropertyNode) => void,
+    actionIfPropertyIsAbsent: () => void = () => {},
+): void {
+    const property = findMatchingProperty(object, propertyNameOrNames);
+
+    if (property !== undefined) {
+        actionIfPropertyIsPresent(property);
+    }
+    else {
+        actionIfPropertyIsAbsent();
+    }
+}
+
+// Utility to insert a new property at the end of a JSON object.
+// The property is inserted even if another property with the same name already exists.
+export function insertProperty(
+    document: Document,
+    editor: DocumentEditor,
+    object: ObjectNode,
+    propertyName: string,
+    propertyValue: string
+): void {
+    const properties = object.properties;
+    const newPropertyAsJsonString = `"${propertyName}": ${propertyValue}`;
+
+    // If the object is empty, the braces are replaced by braces containing the new property.
+    if (properties.length === 0) {
+        editor.replace(object.range, `{ ${newPropertyAsJsonString} }`);
+    }
+
+    // If the object contains a single property, a comma, a space,
+    // and the new property are inserted after that property.
+    else if (properties.length === 1) {
+        const existingProperty = properties[0];
+        editor.insert(existingProperty.range.end, `, ${newPropertyAsJsonString}`);
+    }
+
+    // Otherwise, the content between the two last properties is inserted
+    // at the end of the last property, followed by the new property.
+    else {
+        const lastProperty = properties[properties.length - 1];
+        const secondTolastProperty = properties[properties.length - 2];
+        const contentInBetween = document.getContentInRange(
+            new Range(secondTolastProperty.range.end, lastProperty.range.start)
+        );
+
+        editor.insert(lastProperty.range.end, `${contentInBetween}${newPropertyAsJsonString}`);
+    }
+}
+
+// Utility to delete a property from a JSON object.
+// If no property with one of the given names is found, this function does nothing.
+export function deleteProperty(
+    editor: DocumentEditor,
+    object: ObjectNode,
+    propertyNameOrNames: string | string[]
+): void {
+    const properties = object.properties;
+    const propertyToDeleteIndex = findMatchingPropertyIndex(object, propertyNameOrNames);
+
+    // If no matching property was found, there is nothing to do.
+    if (propertyToDeleteIndex === -1) {
+        return;
+    }
+
+    const nbProperties = properties.length;
+    const lastPropertyIndex = nbProperties - 1;
+    const propertyToDelete = properties[propertyToDeleteIndex];
+
+    // If the property is followed by at least one other property,
+    // delete everything in between the beginning of the property to delete
+    // to the beginning of the next property.
+    if (propertyToDeleteIndex < lastPropertyIndex) {
+        const nextProperty = properties[propertyToDeleteIndex + 1];
+        editor.delete(
+            new Range(propertyToDelete.range.start, nextProperty.range.start)
+        );
+    }
+
+    // Otherwise, it means the property to delete is the last one.
+    else {
+        // If it is the only property, simply delete the property.
+        if (nbProperties === 1) {
+            editor.delete(propertyToDelete.range);
+        }
+        
+        // Otherwise, delete everythingh from the end of the previous property
+        // to the end of the property to delete.
+        else {
+            const previousProperty = properties[propertyToDeleteIndex - 1];
+            editor.delete(
+                new Range(previousProperty.range.end, propertyToDelete.range.end)
+            );
+        }
+    }
+}
--- src/visualisation-providers/vega-marks.ts
+++ src/visualisation-providers/vega-marks.ts
@@ -10,11 +10,15 @@ import { ProgrammableInputMapping } from "../core/mappings/ProgrammableInputMapp
 import { ProgrammableOutputMapping } from "../core/mappings/ProgrammableOutputMapping";
 import { ButtonPopoverRenderer } from "../core/renderers/popover/ButtonPopoverRenderer";
 import { PlotStyle, PlotStyleEditor } from "../core/user-interfaces/plot-style-editor/PlotStyleEditor";
-import { StyleInspector } from "../core/user-interfaces/style-inspector/StyleInspector";
+import { DISABLED_PROPERTY, isEnabledAndDefined } from "../core/user-interfaces/style-inspector/inspectors/SpecialisedStyleInspector";
+import { Style } from "../core/user-interfaces/style-inspector/Style";
+import { StyleInspector, Input as StyleInspectorInput } from "../core/user-interfaces/style-inspector/StyleInspector";
 import { SyntacticCodeVisualisationProvider } from "../core/visualisations/syntactic/SyntacticCodeVisualisationProvider";
 import { Color } from "../utilities/Color";
+import { processProperty, insertProperty, deleteProperty } from "../utilities/languages/json";
+import { ValueWithUnit } from "../utilities/ValueWithUnit";
 
-// TODO: implement this provider!
+
 export const vegaMarksStyleInspectorProvider = new SyntacticCodeVisualisationProvider(
     "Vega marks",
     { languages: ["json"] },
@@ -27,14 +31,272 @@ export const vegaMarksStyleInspectorProvider = new SyntacticCodeVisualisationPro
     [],
     
     new ProgrammableInputMapping(arg => {
+        const pattern = (arg.pattern as SyntacticPattern).node as PropertyNode;
+        const mark = (pattern.value as ObjectNode);
+        // const markProperties = (pattern.value as ObjectNode).properties;
+        
+        // Background style properties.
+        const backgroundStyle: StyleInspectorInput["style"]["background"] = {};
+
+        processProperty(
+            mark,
+            ["fill", "color"],
+            property => {
+                // Only strings are supported.
+                if (property.value.type === "String") {
+                    backgroundStyle.color = Color.fromCss(property.value.value);
+                }
+            }
+        );
+
+        // Border style properties.
+        const borderStyle: StyleInspectorInput["style"]["border"] = {};
+
+        processProperty(
+            mark,
+            ["stroke", "color"],
+            property => {
+                // Only strings are supported.
+                if (property.value.type === "String") {
+                    borderStyle.color = Color.fromCss(property.value.value);
+                }
+            }
+        );
+
+        processProperty(
+            mark,
+            "strokeDash",
+            property => {
+                // Only arrays containing two numbers are supported.
+                if (property.value.type === "Array"
+                && property.value.values.length === 2
+                && property.value.values.some(v => v.type === "Number")) {
+                    const [firstValue, secondValue] = property.value.values as [NumberNode, NumberNode];
+
+                    if (firstValue.value === 1 && secondValue.value === 1) {
+                        borderStyle.type = "dotted";
+                    }
+                    else if (firstValue.value === 2 && secondValue.value === 2) {
+                        borderStyle.type = "dashed";
+                    }
+                    else {
+                        borderStyle.type = [firstValue.value, secondValue.value];
+                    }
+                }
+            },
+            () => {
+                borderStyle.type = "solid";
+            }
+        );
+
+        processProperty(
+            mark,
+            "strokeWidth",
+            property => {
+                // Only numbers are supported.
+                if (property.value.type === "Number") {
+                    borderStyle.thickness = new ValueWithUnit(Number(property.value.value), "px");
+                }
+            },
+            () => {
+                borderStyle.thickness = new ValueWithUnit(0, "px");
+            }
+        );
+
+        // Background style properties.
+        const fontStyle: StyleInspectorInput["style"]["font"] = {};
+
+        processProperty(
+            mark,
+            "font",
+            property => {
+                // Only strings are supported.
+                if (property.value.type === "String") {
+                    fontStyle.family = [property.value.value];
+                }
+            },
+            () => {
+                fontStyle.family = ["sans-serif"]
+            }
+        );
+
+        processProperty(
+            mark,
+            "fontSize",
+            property => {
+                // Only numbers are supported.
+                if (property.value.type === "Number") {
+                    fontStyle.size = new ValueWithUnit(Number(property.value.value), "px");
+                }
+            },
+            () => {
+                fontStyle.size = new ValueWithUnit(11, "px");
+            }
+        );
+
+        processProperty(
+            mark,
+            "fontWeight",
+            property => {
+                // Only strings and numbers are supported.
+                if (property.value.type === "String") {
+                    if (property.value.value === "bold") {
+                        fontStyle.bold = true;
+                    }
+                }
+
+                if (property.value.type === "Number") {
+                    if (property.value.value >= 400) {
+                        fontStyle.bold = true;
+                    }
+                }
+            }
+        );
+
+        processProperty(
+            mark,
+            "fontStyle",
+            property => {
+                // Only strings are supported.
+                if (property.value.type === "String") {
+                    if (property.value.value === "italic") {
+                        fontStyle.italic = true;
+                    }
+                }
+            }
+        );
+
+        const style: StyleInspectorInput["style"] = {};
+        if (Object.getOwnPropertyNames(backgroundStyle).length > 0) {
+            style.background = backgroundStyle;
+        }
+        if (Object.getOwnPropertyNames(borderStyle).length > 0) {
+            style.border = borderStyle;
+        }
+        if (Object.getOwnPropertyNames(fontStyle).length > 0) {
+            style.font = { ...fontStyle, color: DISABLED_PROPERTY };
+        }
+
         return {
-            style: {},
+            style: style,
+            settings: {
+                defaultStyle: {
+                    background: { color: Color.fromHexString("#4682b4") },
+                    font: {
+                        size: new ValueWithUnit(11, "pt"),
+                        family: ["sans-serif"]
+                    }
+                }
+            }
         };
     }),
     new ProgrammableOutputMapping(arg => {
-        
+        const document = arg.document;
+        const editor = arg.output.editor;
+        const pattern = (arg.pattern as SyntacticPattern).node as PropertyNode;
+        const mark = (pattern.value as ObjectNode);
+        const styleChange = arg.output.data.styleChange as Style;
+
+        // Background style properties.
+        const backgroundChange = styleChange.background;
+        if (backgroundChange) {
+            const newColor = backgroundChange.color;
+            if (isEnabledAndDefined(newColor)) {
+                processProperty(
+                    mark,
+                    ["fill", "color"],
+                    property => editor.replace(property.value.range, `"${newColor.css}"`),
+                    () => insertProperty(document, editor, mark, "fill", `"${newColor.css}"`)
+                );
+            }
+        }
+
+        // Border style properties.
+        const borderChange = styleChange.border;
+        if (borderChange) {
+            const newThickness = borderChange.thickness;
+            if (isEnabledAndDefined(newThickness)) {
+                if (newThickness.value === 0) {
+                    deleteProperty(editor, mark, "strokeWidth");
+                }
+                else {
+                    processProperty(
+                        mark,
+                        "strokeWidth",
+                        property => editor.replace(property.value.range, newThickness.value.toString()),
+                        () => insertProperty(document, editor, mark, "strokeWidth", newThickness.value.toString())
+                    );
+                }
+            }
+
+            const newType = borderChange.type;
+            if (isEnabledAndDefined(newType)) {
+                if (newType === "solid") {
+                    deleteProperty(editor, mark, "strokeDash");
+                }
+                else {
+                    const newDashLengths =
+                        newType === "dotted" ? [1, 1] :
+                        newType === "dashed" ? [2, 2] :
+                        newType;
+                    const newDashLengthsAsString = `[${newDashLengths[0]}, ${newDashLengths[1]}]`;
+
+                    processProperty(
+                        mark,
+                        "strokeDash",
+                        property => editor.replace(property.value.range, newDashLengthsAsString),
+                        () => insertProperty(document, editor, mark, "strokeDash", newDashLengthsAsString)
+                    );
+                }
+            }
+
+            const newColor = borderChange.color;
+            if (isEnabledAndDefined(newColor)) {
+                processProperty(
+                    mark,
+                    "stroke",
+                    property => editor.replace(property.value.range, `"${newColor.css}"`),
+                    () => insertProperty(document, editor, mark, "stroke", `"${newColor.css}"`)
+                );
+            }
+        }
+
+        // Font style properties.
+        const fontChange = styleChange.font;
+        if (fontChange) {
+            const newSize = fontChange.size;
+            if (isEnabledAndDefined(newSize)) {
+                processProperty(
+                    mark,
+                    "fontSize",
+                    property => editor.replace(property.value.range, newSize.value.toString()),
+                    () => insertProperty(document, editor, mark, "fontSize", newSize.value.toString())
+                );
+            }
+
+            const newIsBold = fontChange.bold;
+            if (isEnabledAndDefined(newIsBold)) {
+                processProperty(
+                    mark,
+                    "fontWeight",
+                    property => editor.replace(property.value.range, newIsBold ? "true" : "false"),
+                    () => insertProperty(document, editor, mark, "fontWeight", newIsBold ? "true" : "false")
+                );
+            }
+
+            const newIsItalic = fontChange.italic;
+            if (isEnabledAndDefined(newIsItalic)) {
+                processProperty(
+                    mark,
+                    "fontStyle",
+                    property => editor.replace(property.value.range, newIsItalic ? "true" : "false"),
+                    () => insertProperty(document, editor, mark, "fontStyle", newIsItalic ? "true" : "false")
+                );
+            }
+        }
+
+        editor.applyEdits();
     }),
-    
     StyleInspector.makeProvider(),
     ButtonPopoverRenderer.makeProvider({
         buttonContent: "🎨 edit style"
