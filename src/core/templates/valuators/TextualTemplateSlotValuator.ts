638a57705be0e6a4ac744fed626bf1154e1b0b92
Daru13 <daru14@gmail.com>
2023-01-23 21:49:14 +0100

    Add a param. to ignore string delimiters in textual template slot valuators

--- src/core/templates/valuators/TextualTemplateSlotValuator.ts
+++ src/core/templates/valuators/TextualTemplateSlotValuator.ts
@@ -3,13 +3,17 @@ import { TemplateSlotValuatorSettings, TemplateSlotValueType, deriveTemplateSlot
 import { TemplateSlotValuatorProvider } from "./TemplateSlotValuatorProvider";
 
 export interface TemplateSlotTextualValuatorSettings extends TemplateSlotValuatorSettings<TemplateSlotValueType.Text> {
-
+    // List of delimiters that must be trimmed from the start and the end of the value.
+    // The first delimiter of the list will be used to enclose new values if it is defined.
+    ignorePairsOfDelimiters: string[] | false;
 }
 
 export function deriveTemplateSlotTextualValuatorSettingsFromDefaults<
     S extends TemplateSlotValuatorSettings<TemplateSlotValueType.Text>
 >(settings: Partial<S>): TemplateSlotTextualValuatorSettings {
     return {
+        ignorePairsOfDelimiters: ["\"", "'", "`"],
+
         ...deriveTemplateSlotValuatorSettingsFromDefaults(settings)
     };
 }
@@ -32,11 +36,39 @@ export class TemplateSlotTextualValuator extends TemplateSlotValuator<
     }
 
     convertTextToValue(text: string): string {
-        return text;
+        return this.settings.ignorePairsOfDelimiters
+            ? TemplateSlotTextualValuator.trimPairsOfIdentificDelimitersOff(text, this.settings.ignorePairsOfDelimiters)
+            : text;
     }
 
     convertValueToText(newValue: string): string {
+        if (this.settings.ignorePairsOfDelimiters) {
+            const delimiter = this.settings.ignorePairsOfDelimiters[0];
+            return `${delimiter}${newValue}${delimiter}`;
+        }
+
         return newValue;
+    }
+
+    protected static trimPairsOfIdentificDelimitersOff(text: string, delimiters: string[]): string {
+        const textLength = text.length;
+        if (textLength < 2) {
+            console.warn(`The text "${text}" was not trimmed: it contains <2 characters.`);
+            return text;
+        }
+
+        const firstCharacter = text[0];
+        const firstCharacterIsADelimiter = delimiters.includes(firstCharacter);
+        if (firstCharacterIsADelimiter) {
+            const lastCharacter = text[textLength - 1];
+            const lastCharacterIsTheSameDelimiter = lastCharacter === firstCharacter;
+
+            if (lastCharacterIsTheSameDelimiter) {
+                return text.substring(1, textLength - 1);
+            }
+        }
+
+        return text;
     }
 
     static makeProvider(partialSettings: Partial<TemplateSlotTextualValuatorSettings> = {}): TemplateSlotValuatorProvider {
