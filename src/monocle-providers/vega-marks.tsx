6e537959319d9cf2ea6812a44c152b2451b0417d
Daru13 <daru14@gmail.com>
2023-01-31 21:17:17 +0100

    Update the template + form version of the style inspector for Vega marks

--- src/monocle-providers/vega-marks.tsx
+++ src/monocle-providers/vega-marks.tsx
@@ -10,13 +10,10 @@ import { DISABLED_PROPERTY, isEnabledAndDefined } from "../core/user-interfaces/
 import { Style } from "../core/user-interfaces/style-inspector/Style";
 import { StyleInspector, Input as StyleInspectorInput } from "../core/user-interfaces/style-inspector/StyleInspector";
 import { SyntacticMonocleProvider } from "../core/monocles/syntactic/SyntacticMonocleProvider";
-import { BLACK, Color, WHITE } from "../utilities/Color";
+import { Color, } from "../utilities/Color";
 import { processProperty, insertProperty, deleteProperty } from "../utilities/languages/json/json-utilities";
 import { ValueWithUnit } from "../utilities/ValueWithUnit";
-import { InputPrinter } from "../core/user-interfaces/input-printer/InputPrinter";
 import { JsonObjectTemplate } from "../utilities/languages/json/JsonObjectTemplate";
-import { TextualValuator } from "../core/templates/valuators/TextualValuator";
-import { NumericValuator } from "../core/templates/valuators/NumericValuator";
 import { createSlotSpecification, FORM_DATA_TEMPLATE_TRANSFORMERS } from "../core/user-interfaces/form/template-form-utilities";
 import { FormEntryType } from "../core/user-interfaces/form/FormEntry";
 import { Form } from "../core/user-interfaces/form/Form";
@@ -26,6 +23,10 @@ import { StringInput } from "../core/user-interfaces/form/form-elements/StringIn
 import { ButtonGroup } from "../core/user-interfaces/form/form-elements/helpers/ButtonGroup";
 import { Button } from "../core/user-interfaces/form/form-elements/Button";
 import { Select } from "../core/user-interfaces/form/form-elements/Select";
+import { Section } from "../core/user-interfaces/form/form-elements/helpers/Section";
+import { SingleRow } from "../core/user-interfaces/form/form-elements/helpers/SingleRow";
+import { Icon } from "../core/user-interfaces/form/form-elements/helpers/Icon";
+import { AsideRenderer } from "../core/renderers/aside/AsideRenderer";
 
 export const vegaMarksStyleInspectorProvider = new SyntacticMonocleProvider({
     name: "Vega marks",
@@ -329,6 +330,127 @@ const markPropertyTemplate = JsonObjectTemplate.createForAssignmentToKeyNamed(
     { ...FORM_DATA_TEMPLATE_TRANSFORMERS }
 );
 
+function blockOfLength(size: number) {
+    return <div style={{
+        width: size,
+        height: 5,
+        backgroundColor: "rgba(0, 0, 0, 0.7)"
+    }} />;
+}
+
+const form = <>
+    <Section title="Background/fill">
+        <ButtonColorPicker formEntryKey="fill" defaultValue={Color.fromHexString("#4682b4")} label="Fill color" />
+    </Section>
+
+    <Section title="Border/stroke">
+        <SingleRow gapSize="1em">
+            <ButtonColorPicker formEntryKey="stroke" defaultValue={Color.fromHexString("#4682b4")} label="Stroke color" />
+
+            <SingleRow label="Thickness (px)">
+                <NumberInput formEntryKey="strokeWidth" defaultValue={0} min={0} />
+
+                <ButtonGroup>
+                    <Button
+                        formEntryKey="strokeWidth"
+                        text={<Icon id="disable" />}
+                        valueWithType={[0, FormEntryType.Number]}
+                        activateOn={[0, null]}
+                        showWithoutValue={true}
+                    />
+                    <Button
+                        formEntryKey="strokeWidth"
+                        text={blockOfLength(1)}
+                        valueWithType={[1, FormEntryType.Number]}
+                        activateOn={1}
+                        showWithoutValue={true}
+                    />
+                    <Button
+                        formEntryKey="strokeWidth"
+                        text={blockOfLength(3)}
+                        valueWithType={[3, FormEntryType.Number]}
+                        activateOn={3}
+                        showWithoutValue={true}
+                    />
+                    <Button
+                        formEntryKey="strokeWidth"
+                        text={blockOfLength(5)}
+                        valueWithType={[5, FormEntryType.Number]}
+                        activateOn={5}
+                        showWithoutValue={true}
+                    />
+                    <Button
+                        formEntryKey="strokeWidth"
+                        text={blockOfLength(10)}
+                        valueWithType={[10, FormEntryType.Number]}
+                        activateOn={10}
+                        showWithoutValue={true}
+                    />
+                </ButtonGroup>
+            </SingleRow>
+
+            <ButtonGroup label="Line style">
+                <Button
+                    formEntryKey="strokeDash"
+                    text="—"
+                    valueWithType={["", FormEntryType.String]}
+                    activateOn={["solid", null]} showWithoutValue={true}
+                />
+                <Button
+                    formEntryKey="strokeDash"
+                    text="--"
+                    valueWithType={["[2, 2]", FormEntryType.String]}
+                    activateOn="[2, 2]" showWithoutValue={true}
+                />
+                <Button
+                    formEntryKey="strokeDash"
+                    text="···"
+                    valueWithType={["[1, 1]", FormEntryType.String]}
+                    activateOn="[1, 1]" showWithoutValue={true}
+                />
+            </ButtonGroup>
+        </SingleRow>
+    </Section>
+
+    <Section title="Text">
+        <SingleRow gapSize="1em">
+            <SingleRow label="Thickness (px)">
+                <Button<FormEntryType.Number>
+                    formEntryKey="fontSize"
+                    text="–"
+                    valueWithType={v => [v === null ? 10 : Math.max(1, v! - 1), FormEntryType.Number]}
+                    showWithoutValue={true}
+                />
+                <NumberInput formEntryKey="fontSize" defaultValue={11} min={0} hideButtons={true} />
+                <Button<FormEntryType.Number>
+                    formEntryKey="fontSize"
+                    text="+"
+                    valueWithType={v => [v === null ? 12 : v! + 1, FormEntryType.Number]}
+                    showWithoutValue={true}
+                />
+            </SingleRow>
+
+            <Select formEntryKey="font" items={["sans-serif", "serif", "monospace"]} defaultItem="sans-serif" label="Font family" />
+
+            <ButtonGroup>
+                <Button
+                    formEntryKey="fontWeight"
+                    text={<Icon id="bold" />}
+                    valueWithType={v => [v === "normal" || v === null ? "bold" : "normal", FormEntryType.String]}
+                    activateOn="bold" showWithoutValue={true}
+                />
+                <Button
+                    formEntryKey="fontStyle"
+                    text={<Icon id="italic" />}
+                    valueWithType={v => [v === "normal" || v === null ? "italic" : "normal", FormEntryType.String]}
+                    activateOn="italic"
+                    showWithoutValue={true}
+                />
+            </ButtonGroup>
+        </SingleRow>
+    </Section>
+</>;
+
 export const vegaMarksStyleInspectorProvider2 = new SyntacticMonocleProvider({
     name: "Vega marks",
 
@@ -336,37 +458,7 @@ export const vegaMarksStyleInspectorProvider2 = new SyntacticMonocleProvider({
 
     ...markPropertyTemplate.monocleProviderProperties,
 
-    userInterfaceProvider: Form.makeProvider(<>
-        <h3 style={{ textAlign: "center" }}>Style of the mark</h3>
-        <h4>Background/fill:</h4>
-        <ButtonColorPicker formEntryKey="fill" defaultValue={Color.fromHexString("#4682b4")} label="Fill color" />
-
-        <h4>Border/stroke:</h4>
-        <NumberInput formEntryKey="strokeWidth" defaultValue={0} label="Thickness (px)" />
-        <ButtonGroup>
-            <Button formEntryKey="strokeWidth" text="None" valueWithType={[0, FormEntryType.Number]} activateOn={[0, null]} showWithoutValue={true} />
-            <Button formEntryKey="strokeWidth" text="S" valueWithType={[1, FormEntryType.Number]} activateOn={1} showWithoutValue={true} />
-            <Button formEntryKey="strokeWidth" text="M" valueWithType={[3, FormEntryType.Number]} activateOn={3} showWithoutValue={true} />
-            <Button formEntryKey="strokeWidth" text="L" valueWithType={[5, FormEntryType.Number]} activateOn={5} showWithoutValue={true} />
-            <Button formEntryKey="strokeWidth" text="XL" valueWithType={[10, FormEntryType.Number]} activateOn={10} showWithoutValue={true} />
-        </ButtonGroup>
-        
-        <ButtonGroup>
-            <Button formEntryKey="strokeDash" text="Solid" valueWithType={["", FormEntryType.String]} activateOn={["solid", null]} showWithoutValue={true} />
-            <Button formEntryKey="strokeDash" text="Dashed" valueWithType={["[2, 2]", FormEntryType.String]} activateOn="[2, 2]" showWithoutValue={true} />
-            <Button formEntryKey="strokeDash" text="Dotted" valueWithType={["[1, 1]", FormEntryType.String]} activateOn="[1, 1]" showWithoutValue={true} />
-        </ButtonGroup>
-
-        <ButtonColorPicker formEntryKey="stroke" defaultValue={Color.fromHexString("#4682b4")} label="Stroke color" />
-
-        <h4>Text:</h4>
-        <NumberInput formEntryKey="fontSize" defaultValue={11} label="Font size (pt)" />
-        <ButtonGroup>
-            <Button formEntryKey="fontWeight" text="Bold" valueWithType={v => [v === "normal" || v === null ? "bold" : "normal", FormEntryType.String]} activateOn="bold" showWithoutValue={true} />
-            <Button formEntryKey="fontStyle" text="Italic" valueWithType={v => [v === "normal" || v === null ? "italic" : "normal", FormEntryType.String]} activateOn="italic" showWithoutValue={true} />
-        </ButtonGroup>
-        <Select formEntryKey="font" items={["sans-serif", "serif", "monospace"]} defaultItem="sans-serif" label="Font family" />
-    </>),
+    userInterfaceProvider: Form.makeProvider(form),
     
     rendererProvider: ButtonPopoverRenderer.makeProvider({
         buttonContent: "🎨 edit style"
