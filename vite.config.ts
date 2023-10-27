001ddd91abcf8112ad4af1cf1eb3b463657db516
Daru13 <daru14@gmail.com>
2023-10-27 03:47:03 +0200

    Setup a GitHub workflow to build and publish the playground using CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    root: "./src/apps/playground/",
-    plugins: [
-        react()
-    ]
+export default defineConfig(({ command }) => {
+    return {
+        root: "./src/apps/playground/",
+        base: command === "build"
+            ? "/playground/"
+            : "./",
+        plugins: [
+            react()
+        ]
+    };
 });
