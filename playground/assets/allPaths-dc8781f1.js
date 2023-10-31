3d9270c58ef836ed5c646802fdd77f02b36eaa62
github-action <>
2023-10-31 19:14:47 +0000

    Update the playground

---
+++ playground/assets/allPaths-dc8781f1.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-019c9ce9.js";import{I as e}from"./index-533a48ba.js";import{p as r,I as s}from"./index-4448c982.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
