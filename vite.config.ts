46f9392e0a6f9902a43dbaba204b513afd573f30
Daru13 <daru14@gmail.com>
2024-01-31 14:23:59 +0100

    Update dependencies and the app bundling process
    Bundling an app into a single HTML file should be done using Vite (+ a plugin) only. In case of success, using Gulp should become unnecessary, and the Gulpfile and the dependency should be removed in a future commit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(({ command }) => {
     return {
         root: "./src/apps/playground/",
+
         base: command === "build"
             ? "/lorgnette/playground/"
             : "./",
+
         plugins: [
-            react()
-        ]
+            react(),
+            viteSingleFile()
+        ],
+
+        build: {
+            outDir: "./build/",
+            reportCompressedSize: false
+        }
     };
 });
